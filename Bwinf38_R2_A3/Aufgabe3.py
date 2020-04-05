c15fa9a257455ee788cce824037ef458f392c9f2
TPVPfaller <48595504+TPVPfaller@users.noreply.github.com>
2020-04-05 15:29:55 +0200

    Added priority Queue and fixed bugs

--- Bwinf38_R2_A3/Aufgabe3.py
+++ Bwinf38_R2_A3/Aufgabe3.py
@@ -87,24 +87,35 @@ class Solve:
 
     @staticmethod
     def get_gradient(a, b):
-        if b[0] == a[0]:    # vertical
+        if b[0] == a[0]:  # vertical
             if b[1] > a[1]:
-                return 100
+                return float("Inf")
             else:
-                return -100
+                return -float("Inf")
         val = (b[1] - a[1]) / (b[0] - a[0])
-        if val == -0.0 or val == 0.0:   # horizontal
+        if val == -0.0 or val == 0.0:  # horizontal
             return 0
         return val  # diagonally
 
     @staticmethod
-    def dijkstra(graph, start, target):
+    def get_min_dist(dist, queue):
+        minimum = float("Inf")
+        best = -1
+        for v in queue:
+            if dist[v] < minimum:
+                minimum = dist[v]
+                best = v
+        return best
+
+    def dijkstra(self, graph, start, target):
         queue = [start]
+
         visited = set()
         distances = {start: 0}
         parents = {start: None}
         while queue:
-            vertex1 = queue.pop(0)
+            vertex1 = self.get_min_dist(distances, queue)
+            queue.remove(vertex1)
             if vertex1 not in visited:
                 visited.add(vertex1)
                 for vertex2, weight in graph[vertex1]:  # weight := distance between the two vertices
@@ -122,24 +133,32 @@ class Solve:
             cur = parents[cur]
         return distances, path
 
-    def find_path(self, start, target, max_percentage):
+    # Erweiterung: Mehrere Ziele, die alle erreicht werden müssen
+
+    def find_path(self, data, start, target, max_percentage):
         graph = self.connections.get_graph()
         distances, shortest_path = self.dijkstra(graph, start=target, target=start)  # distances relative to target
         max_dist = distances[start] * (1 + max_percentage / 100)
-
         queue = [start]
-        visited = set()
         parents = {start: [Node(node=start, parent=None, gradient=200, turns=0, distance=0)]}
         # dijkstra combined with finding least turns
+        count = 0
         while queue:
-            vertex1 = queue.pop(0)
+            min_turns = []
+            min_turn = float("Inf")
+            for v in queue:
+                if parents[v][0].get_turns() <= min_turn:
+                    min_turn = parents[v][0].get_turns()
+                    min_turns.append(v)
+            max_distance = -1
+            for v in min_turns:
+                if distances[v] > max_distance:
+                    max_distance = distances[v]
+                    vertex1 = v
+            queue.remove(vertex1)
+            count += 1
             for vertex2, weight in graph[vertex1]:
-                visited.add(vertex1)
                 gradient = self.get_gradient(vertex1, vertex2)
-                if vertex2 in visited:
-                    continue
-                if distances[vertex2] + parents[vertex1][0].get_distance() + weight > max_dist:
-                    continue
                 if vertex1 == start:
                     parents[vertex2] = [Node(vertex2, parents[start], gradient, 0, weight)]
                     queue.append(vertex2)
@@ -147,7 +166,7 @@ class Solve:
                     turns = (parents[vertex1][0].get_turns()) + 1
                     total_distance = float("Inf")
                     for e in parents[vertex1]:
-                        if e.get_gradient() == gradient:
+                        if e.get_gradient() == gradient and e.get_distance() + weight + distances[vertex2] <= max_dist:
                             turns -= 1
                             total_distance = e.get_distance() + weight
                             best_node = e
@@ -155,22 +174,34 @@ class Solve:
                         elif total_distance > e.get_distance() + weight:
                             total_distance = e.get_distance() + weight
                             best_node = e
-                    if vertex2 not in parents.keys():
+                    if best_node.get_distance() + weight + distances[vertex2] > max_dist:
+                        continue
+                    elif vertex2 not in parents.keys():
                         parents[vertex2] = [Node(vertex2, best_node, gradient, turns, total_distance)]
-                        queue.append(vertex2)
+                        if vertex2 != target and vertex2 not in queue:
+                            queue.append(vertex2)
                     else:
                         if turns == parents[vertex2][0].get_turns():
                             parents[vertex2].append(Node(vertex2, best_node, gradient, turns, total_distance))
                         elif turns < parents[vertex2][0].get_turns():
-                            parents[vertex2] = [Node(vertex2, best_node, gradient, turns, total_distance)]
+                            parents[vertex2] = [(Node(vertex2, best_node, gradient, turns, total_distance))]
+                            if vertex2 != target and vertex2 not in queue:
+                                queue.append(vertex2)
         # Output
+        print(len(parents[target]))
+        shortest = float("Inf")
+        for i, e in enumerate(parents[target]):
+            if e.get_distance() < shortest:
+                shortest = e.get_distance()
+                index = i
+
         print("Abbiegungen:")
-        print(parents[target][0].get_turns())
+        print(parents[target][index].get_turns())
         print("Distanz:")
-        print(str(parents[target][0].get_distance()) + " (" + str(
-            100 * parents[target][0].get_distance() / distances[start] - 100) + "% longer than the shortest path)")
+        print(str(parents[target][index].get_distance()) + " (" + str(round(
+            100 * parents[target][index].get_distance() / distances[start] - 100, 3)) + "% longer than the shortest path)")
         # create path
-        cur = parents[target][0].get_parent()
+        cur = parents[target][index].get_parent()
         path = [target]
         while type(cur) != list:
             path.append(cur.get_node())
@@ -179,6 +210,34 @@ class Solve:
         print(path)
         path.append(start)
         return path
+
+    # def show_maximal_graph(self, data, start, graph, distances, max_dist):
+    #     queue = [start]
+    #     visited = set()
+    #     result = Graph()
+    #     cost = {start: 0}
+    #     parents = {start: None}
+    #     while queue:
+    #         vertex1 = self.get_min_dist(cost, queue)
+    #         queue.remove(vertex1)
+    #         if vertex1 not in visited:
+    #             visited.add(vertex1)
+    #             for vertex2, weight in graph[vertex1]:  # weight := distance between the two vertices
+    #                 if vertex2 in visited:
+    #                     continue
+    #                 # Updating distance if distance isn't set yet or the distance is shorter than the previous distance
+    #                 if cost.get(vertex2, None) is None or cost.get(vertex1) + weight < cost.get(vertex2):
+    #                     cost[vertex2] = cost.get(vertex1) + weight
+    #                     parents[vertex2] = vertex1
+    #                     queue.append(vertex2)
+    #
+    #     for v1 in graph.keys():
+    #         for v2, weight in graph[v1]:
+    #             if v1 in cost:
+    #                 if cost.get(v1, None) + distances[v2] + weight <= max_dist:
+    #                     result.add_edge(v1, v2, weight)
+    #
+    #     return result.get_graph()
 
 
 class Node:
@@ -215,9 +274,9 @@ time1 = time.time()
 
 d = Data("Beispiel" + example + ".txt")
 s = Solve(d.connections)
-result = s.find_path(d.start, d.target, percent)
+result = s.find_path(d, d.start, d.target, percent)
 
-print('In ' + str((time.time() - time1) * 1000) + ' Milliseconds')
+print('In ' + str((time.time() - time1) * 1000) + ' Milliseconds (with file reading, without graphics)')
 
 # graphical output
 d.draw_path(result)
